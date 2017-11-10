ff3d423ba4274b59d467780858ea09e0ee95137b
user <user@user-PC>
2017-11-10 15:50:34 +0530

    second change

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -4,4 +4,24 @@ public class Employee {
 
 	private int eid;
 	private String ename;
+	public Employee() {
+		super();
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + "]";
+	}
+	
 }
